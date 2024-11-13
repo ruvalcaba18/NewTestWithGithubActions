3f4c514bd10c88b778b31ef68d3f14be7b975271
Jael <jael.ruvalcaba@uabc.edu.mx>
2024-11-13 11:44:02 -0800

    Initial Commit

---
+++ NewTest/NewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTestApp.swift
+//  NewTest
+//
+//  Created by The Coding Kid on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
